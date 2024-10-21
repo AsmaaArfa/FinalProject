3f0791eddf99c79c6ba4ba7b225c1816817cd78b
asmaahesham <aarfa@miu.edu>
2024-10-21 16:33:06 -0500

    commit changes

--- src/Signup.js
+++ src/Signup.js
@@ -26,7 +26,7 @@ function Signup() {
 
   return (
     <div>
-      <h2>Signup</h2>
+      <h2>!!!!Signup!!!!!</h2>
       <form onSubmit={handleSignup}>
         <input type="text" placeholder="Name" value={name} onChange={(e) => setName(e.target.value)} />
         <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
